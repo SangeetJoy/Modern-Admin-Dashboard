2470ba6ab4aa6f9cf378a828844e9fa109fa2601
Sangeet Joy <sangeetjoy@Sangeets-MacBook-Pro.local>
2025-07-20 12:25:51 +0530

    Profile Card component styling refactored

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -6,30 +6,32 @@ const Dashboard = () => {
   return (
     <>
       <TopBar />
-      <ProfileCard
-        name="sangeet joy"
-        role="tester"
-        email="s.joy@gmail.com"
-        imageUrl="src/assets/joy.png"
-      />
-      <ProfileCard
-        name="sangeet joy"
-        role="tester"
-        email="s.joy@gmail.com"
-        imageUrl="src/assets/debanjan.png"
-      />
-      <ProfileCard
-        name="sangeet joy"
-        role="tester"
-        email="s.joy@gmail.com"
-        imageUrl="src/assets/koyel.png"
-      />
-      <ProfileCard
-        name="sangeet joy"
-        role="tester"
-        email="s.joy@gmail.com"
-        imageUrl="src/assets/rohan.png"
-      />
+      <div className="px-9">
+        <ProfileCard
+          name="sangeet joy"
+          role="tester"
+          email="s.joy@gmail.com"
+          imageUrl="src/assets/joy.png"
+        />
+        <ProfileCard
+          name="sangeet joy"
+          role="tester"
+          email="s.joy@gmail.com"
+          imageUrl="src/assets/debanjan.png"
+        />
+        <ProfileCard
+          name="sangeet joy"
+          role="tester"
+          email="s.joy@gmail.com"
+          imageUrl="src/assets/koyel.png"
+        />
+        <ProfileCard
+          name="sangeet joy"
+          role="tester"
+          email="s.joy@gmail.com"
+          imageUrl="src/assets/rohan.png"
+        />
+      </div>
     </>
   );
 };
